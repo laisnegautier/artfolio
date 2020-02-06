e85b640dfd0ab4813b478b7931b2b514280f6674
Gautier <laisne.gautier@gmail.com>
2020-02-06 20:41:47 +0100

    Identity scaffolding

---
+++ artfolio/Areas/Identity/Data/artfolioUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace artfolio.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the artfolioUser class
+    public class artfolioUser : IdentityUser
+    {
+    }
+}
---
+++ artfolio/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using artfolio.Areas.Identity.Data;
+using artfolio.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(artfolio.Areas.Identity.IdentityHostingStartup))]
+namespace artfolio.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<artfolioContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("artfolioContextConnection")));
+
+                services.AddDefaultIdentity<artfolioUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<artfolioContext>();
+            });
+        }
+    }
+}
