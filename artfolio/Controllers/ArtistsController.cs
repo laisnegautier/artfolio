32e05960eb8371cc1428bcc0b14b1764251926d8
Gautier <laisne.gautier@gmail.com>
2020-02-12 11:30:24 +0100

    lastcommit

---
+++ artfolio/Controllers/ArtistsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using artfolio.Data;
+using artfolio.Models;
+
+namespace artfolio.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Artists
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Artists.ToListAsync());
+        }
+
+        // GET: Artists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .FirstOrDefaultAsync(m => m.ArtistId == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // GET: Artists/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ArtistId,Name,PublicLink,Photo,IsPubliclyVisible")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(artist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
+        }
+
+        // GET: Artists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists.FindAsync(id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ArtistId,Name,PublicLink,Photo,IsPubliclyVisible")] Artist artist)
+        {
+            if (id != artist.ArtistId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(artist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtistExists(artist.ArtistId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
+        }
+
+        // GET: Artists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .FirstOrDefaultAsync(m => m.ArtistId == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // POST: Artists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artist = await _context.Artists.FindAsync(id);
+            _context.Artists.Remove(artist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtistExists(int id)
+        {
+            return _context.Artists.Any(e => e.ArtistId == id);
+        }
+    }
+}
