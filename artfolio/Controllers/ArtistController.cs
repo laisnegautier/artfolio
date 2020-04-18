d2ea65d99e90b4ca0e3b682e1f508ebf9d8d3cc6
laisne.gautier <laisne.gautier@gmail.com>
2020-04-18 12:48:23 +0200

    Profile page done

--- artfolio/Controllers/ArtistController.cs
+++ artfolio/Controllers/ArtistController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity;
-using artfolio.Data;
+﻿using artfolio.Data;
 using artfolio.Models;
 using artfolio.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace artfolio.Controllers
 {
@@ -32,13 +30,24 @@ namespace artfolio.Controllers
             Artist artist = await _userManager.FindByNameAsync(userName);
             if (artist == null) return NotFound();
 
+            // Privacy settings
+            if (_signInManager.IsSignedIn(User))
+            {
+                Artist user = await _userManager.GetUserAsync(User);
+                if (user != null && (!artist.IsPubliclyVisible && user != artist)) return NotFound();
+            }
+            else
+            {
+                if (!artist.IsPubliclyVisible) return NotFound();
+            }
+
             ArtistIndexViewModel viewModel = new ArtistIndexViewModel
             {
                 Artist = artist
             };
 
             // Check if the viewer is following the artist
-            if(_signInManager.IsSignedIn(User))
+            if (_signInManager.IsSignedIn(User))
             {
                 Artist user = await _userManager.GetUserAsync(User);
                 Artist toFollow = await _userManager.FindByNameAsync(userName);
@@ -46,7 +55,7 @@ namespace artfolio.Controllers
 
                 ViewData["isFollowing"] = followRelation != null ? true : false;
             }
-            
+
             return View(viewModel);
         }
 
@@ -78,7 +87,7 @@ namespace artfolio.Controllers
         {
             Artist user = await _userManager.GetUserAsync(User);
             Artist toFollow = await _userManager.FindByNameAsync(userName);
-            
+
             FollowRelation followRelation = await _context.FollowRelations.FirstOrDefaultAsync(x => x.FromArtistId == user.Id && x.ToArtistId == toFollow.Id);
             if (followRelation != null)
             {
@@ -120,8 +129,45 @@ namespace artfolio.Controllers
         {
             Artist user = await _userManager.GetUserAsync(User);
             Collection collection = await _context.Collections.FindAsync(id);
-            
+
+            foreach (Artwork artwork in collection.Artworks)
+            {
+                artwork.Collection = null;
+                _context.Artworks.Update(artwork);
+            }
+
             _context.Collections.Remove(collection);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index), user.UserName);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditCollection(Collection viewModel)
+        {
+            Artist user = await _userManager.GetUserAsync(User);
+            Collection collection = await _context.Collections.FindAsync(viewModel.CollectionId);
+
+            collection.Name = viewModel.Name;
+            collection.Description = viewModel.Description;
+
+            _context.Collections.Update(collection);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index), user.UserName);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveArtworkCollection(int CollectionId, int ArtworkId)
+        {
+            Artist user = await _userManager.GetUserAsync(User);
+            Artwork artwork = await _context.Artworks.FindAsync(ArtworkId);
+            Collection collection = await _context.Collections.FindAsync(CollectionId);
+
+            artwork.Collection = null;
+            _context.Artworks.Update(artwork);
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index), user.UserName);
@@ -133,7 +179,7 @@ namespace artfolio.Controllers
         {
             Artist user = await _userManager.GetUserAsync(User);
 
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 Collection collection = await _context.Collections.FindAsync(viewModel.CollectionId);
                 Artwork artworkToUpdate = await _context.Artworks.FindAsync(viewModel.ArtworkId);
@@ -144,6 +190,30 @@ namespace artfolio.Controllers
                 _context.SaveChanges();
             }
             else return NotFound();
+
+            return RedirectToAction(nameof(Index), user.UserName);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUserInfos([Bind("Lastname","Firstname","Description","Nationality","Gender","IsPubliclyVisible")] Artist viewModel)
+        {
+            Artist user = await _userManager.GetUserAsync(User);
+
+            if (ModelState.IsValid)
+            {
+                user.Lastname = viewModel.Lastname;
+                user.Firstname = viewModel.Firstname;
+                user.Description = viewModel.Description;
+                user.Nationality = viewModel.Nationality;
+                user.DateOfBirth = viewModel.DateOfBirth;
+                user.Gender = viewModel.Gender;
+                user.IsPubliclyVisible = viewModel.IsPubliclyVisible;
+
+                //user.DateOfBirth = viewModel.DateOfBirth;
+                await _userManager.UpdateAsync(user);
+                _context.SaveChanges();
+            }
 
             return RedirectToAction(nameof(Index), user.UserName);
         }
---
+++ artfolio/ViewModels/RemoveArtworkCollectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace artfolio.ViewModels
+{
+    public class RemoveArtworkCollectionViewModel
+    {
+        public int CollectionId { get; set; }
+        public int ArtworkId { get; set; }
+        public string ToggleModalRemoveCollection { get; set; }
+    }
+}
